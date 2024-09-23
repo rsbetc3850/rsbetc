a14dbe03a3aa8afe22f21a985207d0fc6bc38677
Your Name <you@example.com>
2024-09-23 20:46:21 +0000

    .

--- app/page.js
+++ app/page.js
@@ -3,15 +3,6 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="page-container">
-      <div className="hero-image-container">
-        <Image
-          src="/overload.jpg" // replace with your image path
-          alt="tech overload"
-          layout="responsive"
-          width={100}
-          height={30}
-        />
-      </div>
       <div className="flex-container">
         <div className="page-title-container">
           <h2 className="page-title">Serving The Nature Coast</h2>
@@ -20,6 +11,15 @@ export default function Home() {
           <p className="section-title">You Can Find It Here!</p>
         </div>
       </div>
+      <div className="w-1/2 md:w-1/3  float-right p-6 m-0">
+          <Image
+            src="/overload.jpg" // replace with your image path
+            alt="tech overload"
+            layout="responsive"
+            width={100}
+            height={100}
+          />
+        </div>
       <div className="">
         <p className="body-text">
           Located at 3850 E Gulf to Lake Highway, Suite 11 in Inverness, FL, we're open Monday through Saturday from 10am to 5pm. Give us a call at <span className="highlight-text">352-344-1962</span> to reach our friendly team.
@@ -27,12 +27,39 @@ export default function Home() {
         <p className="body-text">
           At Batteries Etc., we're more than just a battery store. We're your source for a wide range of high-tech gadgets that you won't find anywhere else. From batteries for devices, cordless phones, wheelchairs, scooters, tools, golf carts, home and garden, to a variety of cables and connectors for audio, video, and data, we've got you covered.
         </p>
+        <div className="w-1/2 md:w-1/3  float-left p-6 m-0">
+          <Image
+            src="/h2o.png" // replace with your image path
+            alt="H2O Wireless"
+            layout="responsive"
+            width={100}
+            height={100}
+          />
+        </div>
         <p className="body-text">
           We're also an H2O Wireless dealer, offering great affordable monthly, multiline, multimonth, and yearly plans for smartphones and flip phones. Plus, we offer Xfinity prepaid wireless with a low monthly price of $45, with no hidden fees or contracts.
         </p>
+        <div className="w-1/2 md:w-1/3  float-right p-6 m-0">
+          <Image
+            src="/cracked.jpg" // replace with your image path
+            alt="Phone Repairs"
+            layout="responsive"
+            width={100}
+            height={100}
+          />
+        </div>
         <p className="body-text">
-          In addition to our product offerings, we also provide repair services for iPhones, iPads, and select Android devices. Our computer services include a full range of hardware and software solutions, and we also perform small electronics repairs.
+          In addition, we also provide repair services for iPhones, iPads, and select Android devices. Our computer services include a full range of hardware and software solutions, and we also perform small electronics repairs.
         </p>
+        <div className="w-1/2 md:w-1/3  float-left p-6 m-0">
+          <Image
+            src="/techteen.jpg" // replace with your image path
+            alt="Phone Repairs"
+            layout="responsive"
+            width={100}
+            height={100}
+          />
+        </div>
         <p className="body-text">
           For the DIYer or hobbyist, we stock a variety of electronics components and tools to help you get the job done right. So whether you're looking for batteries, cables, connectors, wireless plans, repairs, or components, Batteries Etc. is your go-to destination. Come visit us today and see what we have to offer!
         </p>
